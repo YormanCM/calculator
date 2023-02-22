415ae16798d5b7d03b04374f65011013b55cd2e5
Yorman_Carmona <yormancm.17@gmail.com>
2023-02-22 14:19:01 -0500

    1era carga

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent {
+    titulo = 'Aplicación Calculadora';
+    operandoA: number = 1000;
+    operandoB: number = 0;
+    resultado: number = 0; 
+
+    sumar():void{
+      this.resultado = this.operandoA + this.operandoB;
+    }
+
+}
